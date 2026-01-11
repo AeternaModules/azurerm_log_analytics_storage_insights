5267c3824b11765dd4d11d74a3809431fa19f6a7
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:41:22 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "log_analytics_storage_insightss" {
+  description = "All log_analytics_storage_insights resources"
+  value       = azurerm_log_analytics_storage_insights.log_analytics_storage_insightss
+}
+
